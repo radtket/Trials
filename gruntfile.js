e4e9d569c9710c9a064a96948c01fc29e4c0e584
Taylor Radtke <tradtke@fool.com>
2016-08-09 13:02:48 -0400

    fool cleaning

--- gruntfile.js
+++ gruntfile.js
@@ -1,77 +1,84 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Configure task(s)
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            // doesnt need to be called build, we can call it whatever we want 
+            build: {
+                src: ['node_modules/jquery/dist/jquery.js', 'src/js/owl.carousel.min.js', 'src/js/jquery.matchHeight.js', 'src/js/scripts.js'],
+                dest: 'js/script.min.js'
+            },
+
+            dev: {
+                options: {
+                    beautify: true,
+                    mangle: false,
+                    compress: false,
+                    preserveComments: 'all'
+                },
+                src: ['node_modules/jquery/dist/jquery.js', 'src/js/owl.carousel.min.js', 'src/js/jquery.matchHeight.js', 'src/js/scripts.js'],
+                dest: 'js/script.min.js'
+            }   
+        },
 
 
-    uglify : {
-    	// often called 'dist' instead of build
-    	build: {
-    		src: 'src/js/*.js',
-    		dest: 'js/script.min.js'
-    	},
-    	dev: {
-    		options: {
-    			beautify: true,
-    			mangle: false,
-    			compress: false,
-    			preserveComments: 'all'
-    		},
-            files: {
-            'js/script.min.js': ['node_modules/jquery/dist/jquery.js', 'src/js/slick.js', 'src/js/jquery.balanced-gallery.js' ,'src/js/jquery.matchHeight.js', 'src/js/scripts.js']
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['uglify:dev']
+            },
+                css: {
+                files: ['src/scss/**/*.scss'], // <-- when these files change
+                tasks: ['sass:dev'] // <-- run this task
+            },
+            html: {
+                files: ['*.html']
             }
-    	},
-    },
+        },
 
 
-    watch : {
-	    options: {
-            livereload: true,
-	    },
-    	js: {
-    		files: ['src/js/**/*.js'], // <-- when these files change
-    		tasks: ['uglify:dev']	// <-- run this task
-    	},
-    	css: {
-    		files: ['src/scss/**/*.scss'], // <-- when these files change
-    		tasks: ['sass:dev']	// <-- run this task
-    	},
-        html: {
-            files: ['*.html']
-        }
-    },
+
+        sass: {
+            dev: {
+                options: {
+                    outputStyle: 'expanded'
+                },
+                files: {
+                    'css/style.css' : 'src/scss/style.scss'
+                }
+            },
+
+            build: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/style.css' : 'src/scss/style.scss'
+                }
+            }
+        },
 
 
-    sass: {
-    	dev: {
-    		options: {
-    			outputStyle: 'expanded'
-    		},
-    		files: {
-                'css/style.css'  :  'src/scss/style.scss'
-    		}
-    	},
-    	build: {
-    		options: {
-    			outputStyle: 'compressed'
-    		},
-    		files: {
-    			'css/style.css'  :  'src/scss/style.scss'
-    		}
-    	}
-    }
 
- 
 
-  });
 
-  // Load the Grunt plugins.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch'); // watch you do not need to resgister that task just enter on comand line 'grunt watch'
-  grunt.loadNpmTasks('grunt-sass');
 
-  // Register the default tasks.
-  grunt.registerTask('default', ['uglify:dev', 'sass:dev']);
-  grunt.registerTask('build', ['uglify:build', 'sass:build']);
+    });
+
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+
+    // Register tasks(s)
+    grunt.registerTask('default', ['uglify:dev', 'sass:dev']);
+    grunt.registerTask('build', ['uglify:build', 'sass:build']);
+
 };
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,35 +1,32 @@
-$(document).ready(function(){
- 
-    var window_height = $(window).height(),
-    window_width = $(window).width(),
-    navigation_height;
+
+$(document).ready(function() {
+  $(window).trigger("resize");
+  initPageSliders();
+});
+
+$(window).resize(function() {
+  js_height_init();
+  initEqualHeight();
+});
+
+
+  var window_height = $(window).height();
+  var window_width = $(window).width();
+  var navigation_height;
+
+
+  $(window).resize(function() {
+    window_height = $(window).height();
+    navigation_height = $(".navigation").height();
+    $('#header-wrap').css('height', (window_height - (navigation_height / 2)) + 'px');
+    $('#header-wrap .logo').css('width', $('#header-wrap .container').width() + 'px');
+  }).resize();
 
 
 
-      /* ============== SLIDERS ============== */
 
 
-
-  $('#header-slide').slick({
-      dots: true,
-      autoplay: true,
-      autoplaySpeed: 8000,
-      mobileFirst: true,
-    });
-
-      $(window).resize(function() {
-
-        window_height = $(window).height();
-
-        navigation_height = $(".navigation").height();
-
-        $('#header-wrap').css('height', (window_height - (navigation_height / 2)) + 'px');
-        $('#header-wrap .logo').css('width', $('#header-wrap .container').width() + 'px');
-
-      }).resize();
-
-      /* ============== MENU ============== */
-
+  /* ============== MENU ============== */
 
   $('#menulink').click(function(event) {
     event.preventDefault();
@@ -38,52 +35,157 @@ $(document).ready(function(){
       $('.navigation-wrapper').removeClass('show-menu');
       $('.navigation').hide();
       $('.navigation li').removeClass('small-padding');
-    } else {
+    } 
+    else {
       $('.navigation-wrapper').addClass('show-menu');
       $('.navigation').fadeIn();
       $('.navigation li').addClass('small-padding');
      }
   });
 
-(function() {
-
-  /* matchHeight example */
-
-  $(function() {
-    // apply your matchHeight on DOM ready (they will be automatically re-applied on load or resize)
 
 
 
-    // apply matchHeight to each item container's items
-    $('.items-container').each(function() {
-      $(this).children('.item').matchHeight();
+
+/* ---------------------------------------------
+Height 100%
+--------------------------------------------- */
+function js_height_init() {
+  (function($) {
+    $(".js-height-full").height($(window).height());
+    $(".js-height-parent").each(function() {
+      $(this).height($(this).parent().first().height());
     });
+  })(jQuery);
+}
 
-    // test property
-    $('.property-items .item').matchHeight({
-      property: 'min-height'
-    });
 
-    // test target
-    $('.target-items').each(function() {
-      $(this).children('.item-0, .item-2, .item-3').matchHeight({
-        target: $(this).find('.item-1')
-      });
-    });
 
-    // example of update callbacks (uncomment to test)
-    $.fn.matchHeight._beforeUpdate = function(event, groups) {
-      //var eventType = event ? event.type + ' event, ' : '';
-      //console.log("beforeUpdate, " + eventType + groups.length + " groups");
-    }
-
-    $.fn.matchHeight._afterUpdate = function(event, groups) {
-      //var eventType = event ? event.type + ' event, ' : '';
-      //console.log("afterUpdate, " + eventType + groups.length + " groups");
-    }
-  });
-
-})();
-  
-  
+/* ---------------------------------------------
+Sections Data-Backgrounds
+--------------------------------------------- */
+var pageSection = $(".home-section, .page-section, .small-section, .split-section");
+pageSection.each(function(indx) {
+  if ($(this).attr("data-background")) {
+    $(this).css("background-image", "url(" + $(this).data("background") + ")");
+  }
 });
+
+
+
+// Equal Height Plugin
+
+function initEqualHeight() {
+  $('.items-container').each(function() {
+    $(this).children('.item').matchHeight({
+    property: 'min-height'
+    });
+  });
+}
+
+
+
+  
+/* ---------------------------------------------
+Owl Carousel Sliders 
+--------------------------------------------- */
+function initPageSliders() {
+  (function($) {
+    "use strict";
+
+    // Fullwidth slideshow
+
+    var sync1 = $(".fullwidth-slideshow");
+    var sync2 = $(".fullwidth-slideshow-pager");
+
+    $(".fullwidth-slideshow").owlCarousel({
+      autoPlay: 5000,
+      stopOnHover: true,
+      transitionStyle: "fade",
+      slideSpeed: 350,
+      singleItem: true,
+      autoHeight: true,
+      pagination: false,
+      navigation: true,
+      navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+      afterAction: syncPosition,
+      responsiveRefreshRate: 200
+    });
+
+    $(".fullwidth-slideshow-pager").owlCarousel({
+      autoPlay: 5000,
+      stopOnHover: true,
+      items: 8,
+      itemsDesktop: [1199, 8],
+      itemsDesktopSmall: [979, 7],
+      itemsTablet: [768, 6],
+      itemsMobile: [480, 4],
+      autoHeight: true,
+      pagination: false,
+      navigation: false,
+      responsiveRefreshRate: 100,
+      afterInit: function(el) {
+        el.find(".owl-item").eq(0).addClass("synced");
+      }
+    });
+
+    function syncPosition(el) {
+      var current = this.currentItem;
+      $(".fullwidth-slideshow-pager").find(".owl-item").removeClass("synced").eq(current).addClass("synced")
+      if ($(".fullwidth-slideshow-pager").data("owlCarousel") !== undefined) {
+        center(current)
+      }
+    }
+
+    $(".fullwidth-slideshow-pager").on("click", ".owl-item", function(e) {
+      e.preventDefault();
+      var number = $(this).data("owlItem");
+      sync1.trigger("owl.goTo", number);
+    });
+
+    function center(number) {
+      var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+      var num = number;
+      var found = false;
+      for (var i in sync2visible) {
+        if (num === sync2visible[i]) {
+          var found = true;
+        }
+      }
+      if (found === false) {
+        if (num > sync2visible[sync2visible.length - 1]) {
+          sync2.trigger("owl.goTo", num - sync2visible.length + 2)
+        } else {
+          if (num - 1 === -1) {
+            num = 0;
+          }
+          sync2.trigger("owl.goTo", num);
+        }
+      } else
+      if (num === sync2visible[sync2visible.length - 1]) {
+        sync2.trigger("owl.goTo", sync2visible[1])
+      } else
+      if (num === sync2visible[0]) {
+        sync2.trigger("owl.goTo", num - 1)
+      }
+    }
+
+    var owl = $(".fullwidth-slideshow").data("owlCarousel");
+
+    $(document.documentElement).keyup(function(event) {
+      // handle cursor keys
+      if (event.keyCode == 37) {
+        owl.prev();
+      } else
+      if (event.keyCode == 39) {
+        owl.next();
+      }
+    });
+
+    if ($(".owl-carousel").length) {
+      var owl = $(".owl-carousel").data('owlCarousel');
+      owl.reinit();
+    }
+
+  })(jQuery);
+};
